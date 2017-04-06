161acca8e48e37dd8b1e58cd8e3dc04daf0d1f3a
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-04-06 23:35:55 +0100

    Initial listTask tests

---
+++ test/tasks/listTask.test.js
@@ -0,0 +1,34 @@
+import listTask from '../../src/tasks/listTask';
+
+describe('listTask', () => {
+    it('should contain expected description', () => {
+        expect(listTask.description).to.equal('Lists all tasks');
+    });
+
+    it('should contain expected commands', () => {
+        expect(listTask.commands).to.deep.equal(['list', 'commands']);
+    });
+
+    describe('when executing task', () => {
+        const defaultTaskList = [];
+        const fakeBot = {};
+
+        let spyRespondInChannel;
+
+        beforeEach(() => {
+            spyRespondInChannel = sinon.spy();
+            fakeBot.respondInChannel = spyRespondInChannel;
+        });
+
+        it('should call respondInChannel with expected channel', () => {
+            const givenChannel = 'C1234';
+            const givenMessage = {
+                channel: givenChannel
+            };
+
+            listTask.execute(fakeBot, givenMessage, defaultTaskList);
+
+            expect(spyRespondInChannel.getCall(0).args[0]).to.equal(givenChannel);
+        });
+    });
+});
